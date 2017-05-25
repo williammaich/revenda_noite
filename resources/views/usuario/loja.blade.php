5313dbbba76ff4cece180ada3ad5b584f0fc53a4
William Kolosque <williammaich@hotmail.com>
2017-05-24 22:33:34 -0300

    criado dois arquivos

--- resources/views/usuario/loja.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: William
- * Date: 24/05/2017
- * Time: 21:46
- */
---
+++ resources/views/usuario/titulo.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Loja revenda herbie</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Revenda Herbie</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Carros</a></li>
+            <li><a href="#">Marcas</a></li>
+            <li><a href="#">Propostas</a></li>
+        </ul>
+    </div>
+</nav>
+
+
+@yield
+
+</body>
+</html>
