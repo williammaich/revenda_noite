82fec40b9b062e8ef9d600a48e8c8e6fd9521c95
William Kolosque <williammaich@hotmail.com>
2017-05-25 18:42:23 -0300

    volta das rotas

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,36 @@
 
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('index');
+//});
+
+Route::resource('carros', 'CarroController');
+Route::resource('marcas','MarcaController');
+Route::get('carrosfoto/{id}', 'CarroController@foto')
+    ->name('carros.foto');
+Route::post('carrosfotostore', 'CarroController@storeFoto')
+    ->name('carros.store.foto');
+Route::get('carrospesq', 'CarroController@pesq')
+    ->name('carros.pesq');
+Route::post('carrosfiltros', 'CarroController@filtros')
+    ->name('carros.filtros');
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index');
+
+Route::get('register', function() {
+    return "<h1> Acesso Restrito </h1>";
+});
